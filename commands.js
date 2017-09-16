eee4458e5e2294af952199d7f06c433472ae83e4
H Camilo G Ruiz <hcamilogruiz@MB-Air.local>
2017-09-16 15:26:24 -0500

    node course

---
+++ commands.js
@@ -0,0 +1,35 @@
+// Install brew
+// https://brew.sh/
+
+// Install postgreSql
+// brew install postgresql
+// brew services start postgresql
+
+// Install redis
+// brew install redis
+// brew services start redis
+
+// Install ansible
+// brew install ansible
+
+// Install vagrant
+// https://www.vagrantup.com/
+
+// npm i --save-dev standard
+// Crear script para ejecutar standard desde npm
+// npm run lint
+// Para ejecutar comandos desde un script se debe acceder a traves de --
+// npm run lint -- --fix
+
+// npm i sequelize pg pg-hstore --save
+
+// DATA BASE PostgreSQL
+// pslq postgresql
+// CREATE ROL platzi WITH LOGIN PASSWORD 'platzi';
+// CREATE DATABASE platziverse;
+// GRAN ALL PRIVILEGES ON DATABASE platziverse TO platzi;
+
+// npm i debug --save
+
+// npm i inquirer chalk --save
+//
